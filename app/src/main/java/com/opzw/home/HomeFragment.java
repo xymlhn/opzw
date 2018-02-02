279770ac5fd2d3dea2d4bbb404bfef71f81a369e
Cartman <642815752@qq.com>
2018-02-02 16:14:02 +0800

    轮播图

--- app/src/main/java/com/opzw/home/HomeFragment.java
+++ app/src/main/java/com/opzw/home/HomeFragment.java
@@ -1,10 +1,20 @@
 package com.opzw.home;
 
+import android.graphics.Color;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 
 import com.opzw.R;
 import com.opzw.base.BaseFragment;
 import com.opzw.guide.nav.OnTabReselectListener;
+import com.opzw.order.ExamplePagerAdapter;
+
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.ViewPagerHelper;
+import net.lucode.hackware.magicindicator.buildins.circlenavigator.CircleNavigator;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 文 件 名: TestFragment
@@ -17,6 +27,11 @@ import com.opzw.guide.nav.OnTabReselectListener;
 
 
 public class HomeFragment extends BaseFragment implements OnTabReselectListener {
+    private static final Integer[] CHANNELS = new Integer[]{R.mipmap.banner_1, R.mipmap.banner_2};
+    private List<Integer> mDataList = Arrays.asList(CHANNELS);
+    private HomePagerAdapter mExamplePagerAdapter = new HomePagerAdapter(mDataList);
+
+    private ViewPager mViewPager;
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_home;
@@ -25,8 +40,24 @@ public class HomeFragment extends BaseFragment implements OnTabReselectListener 
     @Override
     protected void initWidget(View root) {
         super.initWidget(root);
+        mViewPager = root.findViewById(R.id.view_pager);
+        mViewPager.setAdapter(mExamplePagerAdapter);
+        initMagicIndicator1(root);
     }
-
+    private void initMagicIndicator1(View root) {
+        MagicIndicator magicIndicator = root.findViewById(R.id.magic_indicator1);
+        CircleNavigator circleNavigator = new CircleNavigator(getContext());
+        circleNavigator.setCircleCount(CHANNELS.length);
+        circleNavigator.setCircleColor(Color.WHITE);
+        circleNavigator.setCircleClickListener(new CircleNavigator.OnCircleClickListener() {
+            @Override
+            public void onClick(int index) {
+                mViewPager.setCurrentItem(index);
+            }
+        });
+        magicIndicator.setNavigator(circleNavigator);
+        ViewPagerHelper.bind(magicIndicator, mViewPager);
+    }
     @Override
     public void onTabReselect() {
 
---
+++ app/src/main/java/com/opzw/home/HomePagerAdapter.java
@@ -0,0 +1,58 @@
+package com.opzw.home;
+
+import android.graphics.Color;
+import android.support.v4.view.PagerAdapter;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by hackware on 2016/9/10.
+ */
+
+public class HomePagerAdapter extends PagerAdapter {
+    private List<Integer> mDataList;
+
+    public HomePagerAdapter(List<Integer> dataList) {
+        mDataList = dataList;
+    }
+
+    @Override
+    public int getCount() {
+        return mDataList == null ? 0 : mDataList.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        ImageView imageView = new ImageView(container.getContext());
+        imageView.setImageResource(mDataList.get(position));
+        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        container.addView(imageView);
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public int getItemPosition(Object object) {
+        TextView textView = (TextView) object;
+        String text = textView.getText().toString();
+        int index = mDataList.indexOf(text);
+        if (index >= 0) {
+            return index;
+        }
+        return POSITION_NONE;
+    }
+}
